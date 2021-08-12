ee7b5b02615c0cf5aea9a2d1b5e6c7d85fff28bb
Httohu <huyuantao@outlook.com>
2021-08-12 12:20:56 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -17,45 +17,47 @@ namespace htto_judger
 {
     string compile(const string &compiler, const JudgeInfo &judge_info);
     void children_run(const JudgeInfo &info);
-    ResultTag get_state(int stat,const rusage &usage,const htto_judger::JudgeInfo & info)
+    ResultTag get_state(int stat, const rusage &usage, const htto_judger::JudgeInfo &info)
     {
-        int usage_time=usage.ru_utime.tv_sec*1000 + usage.ru_utime.tv_usec/1000;
-        if(usage_time > info.time_limit)
+        int usage_time = usage.ru_utime.tv_sec * 1000 + usage.ru_utime.tv_usec / 1000;
+        if (usage_time > info.time_limit)
             return ResultTag::TLE;
 
-        if(info.memory_limit*1024*1024 < usage.ru_maxrss)
+        if (info.memory_limit * 1024 * 1024 < usage.ru_maxrss)
             return ResultTag::MLE;
-        
-        if (WIFEXITED(stat) ) 
+
+        if (WIFEXITED(stat))
         {
-            int exit_code=WEXITSTATUS(stat);
-            if(!exit_code)
+            int exit_code = WEXITSTATUS(stat);
+            if (!exit_code)
                 return ResultTag::NORMAL;
-            if(exit_code==ResultTag::CE)
+            if (exit_code == ResultTag::CE)
                 return (ResultTag)exit_code;
             return ResultTag::UKE;
         }
 
-        if(WIFSIGNALED(stat))
+        if (WIFSIGNALED(stat))
         {
             return ResultTag::RE;
         }
         return ResultTag::UKE;
     }
-    
-    struct __KillerParam{
+
+    struct __KillerParam
+    {
         int child_id;
         JudgeInfo *info;
     };
-    void* killer(void *param){
-        __KillerParam kp=*(__KillerParam*)param;
-        int sleep_time=kp.info->time_limit/1000+2;
+    void *killer(void *param)
+    {
+        __KillerParam kp = *(__KillerParam *)param;
+        int sleep_time = kp.info->time_limit / 1000 + 2;
         sleep(sleep_time);
-        kill(kp.child_id,SIGKILL);
+        kill(kp.child_id, SIGKILL);
         printf("killed by killer\n");
         return nullptr;
     }
-} 
+}
 
 /*
 crate test/xxx and test/1.in 
@@ -64,12 +66,13 @@ merdog: ./a.out 10 1000 merdog ../test/1.in ../test/1.out ../test/main.mer 111 .
 python: ./a.out 10 1000 python ../test/1.in ../test/1.out ../test/main.py 111 ../test/
 JS: ./a.out 10 1000 node ../test/1.in ../test/1.out ../test/main.js 111 ../test/
 */
-struct JudgerData{
+struct JudgerData
+{
     int usage_time;
     int usage_mem;
 };
 
-void write_judger_data_to_pipe(const std::string &pipe_name,JudgerData judger_data)
+void write_judger_data_to_pipe(const std::string &pipe_name, JudgerData judger_data)
 {
     const int open_mod = O_WRONLY;
 
@@ -100,41 +103,45 @@ void write_judger_data_to_pipe(const std::string &pipe_name,JudgerData judger_da
 }
 
 int main(int argc, char **argv)
-{   
-    auto info=htto_judger::get_judge_info(argc, argv);
-    htto_judger::compile(info.lang,info);
-    pid_t child_pid=fork();
-    if(child_pid<0)
+{
+    auto info = htto_judger::get_judge_info(argc, argv);
+    htto_judger::compile(info.lang, info);
+    pid_t child_pid = fork();
+    if (child_pid < 0)
     {
-        cerr<<"create thread error\n";
+        cerr << "create thread error\n";
         exit(1);
     }
     // children process
-    if(child_pid==0)
+    if (child_pid == 0)
     {
         htto_judger::children_run(info);
     }
     // parent process
-    else {
+    else
+    {
         pthread_t moniter_thread;
         htto_judger::__KillerParam param;
-        param.child_id=child_pid;
+        param.child_id = child_pid;
         param.info = &info;
         int t = pthread_create(&moniter_thread, NULL, htto_judger::killer, (void *)&param);
-        if (t != 0) {
+        if (t != 0)
+        {
             exit(-1);
         }
         rusage cost;
         int stat;
-        wait4(child_pid,&stat,WSTOPPED,&cost);
+        wait4(child_pid, &stat, WSTOPPED, &cost);
         pthread_cancel(moniter_thread);
-        int usage_time=cost.ru_utime.tv_sec*1000 + cost.ru_utime.tv_usec/1000;
+        int usage_time = cost.ru_utime.tv_sec * 1000 + cost.ru_utime.tv_usec / 1000;
+#ifdef __APPLE__
+        JudgerData judger_res = {usage_time, (int)cost.ru_maxrss / 1024};
+#else
+        JudgerData judger_res = {usage_time, (int)cost.ru_maxrss};
+#endif
+        write_judger_data_to_pipe("/tmp/pipe" + info.submission_id, judger_res);
 
-        
-        JudgerData judger_res={usage_time,cost.ru_maxrss};
-        write_judger_data_to_pipe("/tmp/pipe"+info.submission_id,judger_res);
-
-        int type=htto_judger::get_state(stat,cost,info);
+        int type = htto_judger::get_state(stat, cost, info);
         exit(type);
     }
     return 0;
